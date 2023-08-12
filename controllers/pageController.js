eb060c237d92efee222aa9f38e21a6a1e80c010d
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-08-12 18:05:11 +0300

    contact message to email

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,9 +1,11 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
-    res.status(200).render("index", {
-      page_name: "index",
-    });
-  };
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+};
 
 exports.getAboutPage = (req, res) => {
   res.status(200).render("about", {
@@ -21,4 +23,57 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render("login", {
     page_name: "login",
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+};
+
+exports.sendEmail =  async (req, res) => {
+  const outputMessage = `
+  
+  <h1>Mail Detail</h1>
+  <ul>
+  <li>Name: ${req.body.name}</li>
+  <li>Email: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>  ${req.body.message} </p>
+  
+  `;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+      user: "karadumanyazilim@gmail.com",
+      pass: "hcnflysbdwiuwhdw",
+    },
+  });
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <karadumanyazilim@gmail.com>', // sender address
+      to: "karadumancagatay@gmail.com", // list of receivers
+      subject: "Smart EDU Contact Form New Messahe ✔", // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    //
+    // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+    //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+    //       <https://github.com/forwardemail/preview-email>
+    //
+  }
+
+  res.status(200).redirect("/contact");
 };
