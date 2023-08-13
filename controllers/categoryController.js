251e52b31a805df2d602ad87c2a5fa123bcc9927
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-08-13 15:41:10 +0300

    created admin authorization

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,14 +3,25 @@ const Category = require("../models/Category");
 exports.createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: "success",
-      category,
-    });
+    req.flash("success", `${category.name} category has been created successfully.`);
+    res.status(201).redirect("/users/dashboard")
   } catch (error) {
     res.status(400).json({
       status: "fail",
       error,
     });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndRemove(req.params.id);
+    req.flash("error", `${category.name} category has been removed successfully.`);
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    }); 
   }
 };
